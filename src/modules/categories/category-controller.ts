55d684a7c28783a8aa7b579e0d84d4f13cc80cef
Shravan <shravansinghchaudhary@gmail.com>
2024-09-24 19:51:25 +0530

    feat: :sparkles: Add asyncHandler utility functionr and refactor controller

--- src/modules/categories/category-controller.ts
+++ src/modules/categories/category-controller.ts
@@ -1,7 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { validationResult } from "express-validator";
 import { CreateHttpError, httpResponse, HttpStatus } from "../../common/http";
-import createHttpError from "http-errors";
 import { ICategory } from "./types";
 import { CategoryService } from "./category-service";
 import ResponseMessage from "../../common/constants/responseMessage";
@@ -13,33 +12,25 @@ export class CategoryController {
         private logger: Logger
     ) {}
 
-    async create(req: Request, res: Response, next: NextFunction) {
+    async create(req: Request, res: Response, _next: NextFunction) {
         this.logger.info("CONTROLLER_REQUEST", {
             meta: req.body as ICategory
         });
-        try {
-            const result = validationResult(req);
-            if (!result.isEmpty()) {
-                const err = CreateHttpError.BadRequestError(result.array()[0].msg as string);
-                throw err;
-            }
 
-            const { name, priceConfiguration, attributes } = req.body as ICategory;
-
-            const category = await this.categoryService.create({
-                name,
-                priceConfiguration,
-                attributes
-            });
-
-            httpResponse(req, res, HttpStatus.CREATED, ResponseMessage.CREATED, category);
-        } catch (error) {
-            if (error instanceof createHttpError.HttpError) {
-                next(error);
-                return;
-            }
-            next(CreateHttpError.InternalServerError());
-            return;
+        const result = validationResult(req);
+        if (!result.isEmpty()) {
+            const err = CreateHttpError.BadRequestError(result.array()[0].msg as string);
+            throw err;
         }
+
+        const { name, priceConfiguration, attributes } = req.body as ICategory;
+
+        const category = await this.categoryService.create({
+            name,
+            priceConfiguration,
+            attributes
+        });
+
+        httpResponse(req, res, HttpStatus.CREATED, ResponseMessage.CREATED, category);
     }
 }
--- src/modules/categories/category-router.ts
+++ src/modules/categories/category-router.ts
@@ -1,16 +1,20 @@
-import express, { Request, Response, NextFunction, RequestHandler } from "express";
+import express, { Request, Response, NextFunction } from "express";
 import { CategoryController } from "./category-controller";
 import categoryValidator from "./category-validator";
 import { CategoryService } from "./category-service";
 import logger from "../../config/logger";
-
+import asyncHandler from "../../common/utils/asyncHandler";
 const router = express.Router();
 
 const categoryService = new CategoryService();
 const categoryController = new CategoryController(categoryService, logger);
 
-router.post("/", categoryValidator, (async (req: Request, res: Response, next: NextFunction) => {
-    await categoryController.create(req, res, next);
-}) as unknown as RequestHandler);
+router.post(
+    "/",
+    categoryValidator,
+    asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+        await categoryController.create(req, res, next);
+    })
+);
 
 export default router;
