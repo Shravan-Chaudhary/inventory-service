f729c9995e6419d8f707e638ffb69aac1c93e8a8
Shravan <shravansinghchaudhary@gmail.com>
2024-09-24 15:27:47 +0530

    feat: :card_file_box: Add category model definition

---
+++ src/modules/categories/category-model.ts
@@ -0,0 +1,59 @@
+import mongoose from "mongoose";
+import {
+    IAttributes,
+    ICategory,
+    IPriceConfiguration,
+    EPricetype,
+    EWidgetType
+} from "./types";
+
+const attributeSchema = new mongoose.Schema<IAttributes>({
+    name: {
+        type: String,
+        required: true
+    },
+    widgetType: {
+        type: String,
+        enum: [EWidgetType.SWITCH, EWidgetType.RADIO],
+        required: true
+    },
+    defaultValue: {
+        type: mongoose.Schema.Types.Mixed,
+        required: true
+    },
+    availableOptions: {
+        type: [String],
+        required: true
+    }
+});
+
+const priceConfigurationSchema = new mongoose.Schema<IPriceConfiguration>({
+    priceType: {
+        type: String,
+        enum: [EPricetype.BASE, EPricetype.ADDITIONAL],
+        required: true
+    },
+    availableOptions: {
+        type: [String],
+        required: true
+    }
+});
+
+const categorySchema = new mongoose.Schema<ICategory>({
+    name: {
+        type: String,
+        required: true
+    },
+    priceConfiguration: {
+        type: Map,
+        of: priceConfigurationSchema,
+        required: true
+    },
+    attributes: {
+        type: [attributeSchema],
+        required: true
+    }
+});
+
+const Category = mongoose.model<ICategory>("Category", categorySchema);
+export default Category;
