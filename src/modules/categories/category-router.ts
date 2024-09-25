f9e527c934057b6b5afe83f599c7e803682117aa
Shravan <shravansinghchaudhary@gmail.com>
2024-09-26 03:43:01 +0530

    feat: :necktie: Create authorization middleware and add it to category router

--- src/modules/categories/category-router.ts
+++ src/modules/categories/category-router.ts
@@ -5,6 +5,8 @@ import { CategoryService } from "./category-service";
 import logger from "../../config/logger";
 import asyncHandler from "../../common/utils/asyncHandler";
 import authenticate from "../../common/middlewares/authenticate";
+import canAccess from "../../common/middlewares/canAccess";
+import { Roles } from "../../constants";
 const router = express.Router();
 
 const categoryService = new CategoryService();
@@ -13,6 +15,7 @@ const categoryController = new CategoryController(categoryService, logger);
 router.post(
     "/",
     authenticate,
+    canAccess([Roles.ADMIN]),
     categoryValidator,
     asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
         await categoryController.create(req, res, next);
