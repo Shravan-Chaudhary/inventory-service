bda18491b51e64b5af9a7ea9daa9bcb1b6a1f933
Shravan <shravansinghchaudhary@gmail.com>
2024-09-26 02:00:29 +0530

    fix: :ambulance: Fix global error handler response

--- src/common/middlewares/globalErrorHandler.ts
+++ src/common/middlewares/globalErrorHandler.ts
@@ -1,8 +1,9 @@
 import { HttpError } from "http-errors";
 import { NextFunction, Request, Response } from "express";
 import createErrorObject from "../utils/errorObject";
+// import { httpResponse } from "../http";
 
 export const globalErrorHandler = (err: HttpError, req: Request, res: Response, _next: NextFunction) => {
     const errorObject = createErrorObject(err, req);
-    res.status(err.statusCode).json(errorObject);
+    res.status(errorObject.statusCode).json(errorObject);
 };
