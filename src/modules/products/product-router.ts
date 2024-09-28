d06a9295887fb6db3051c1bc6fd30047e3677bac
Shravan <shravansinghchaudhary@gmail.com>
2024-09-28 22:09:22 +0530

    feat: :necktie: Add file upload middleware to product router

--- src/modules/products/product-router.ts
+++ src/modules/products/product-router.ts
@@ -4,15 +4,18 @@ import { ProductService } from "./product-service";
 import { ProductController } from "./product-controller";
 import createProductValidator from "./validators/create-product-validator";
 import logger from "../../config/logger";
+import fileUpload from "express-fileupload";
 
 const router = express.Router();
 
 const productService = new ProductService();
 const productController = new ProductController(productService, logger);
 
+//TODO: Add File upload middleware
 router.post(
     "/",
     createProductValidator,
+    fileUpload(),
     asyncHandler(async (req, res) => {
         await productController.create(req, res);
     })
