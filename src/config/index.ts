82e2b360817f493364270f3d593a8a014d04d302
Shravan <shravansinghchaudhary@gmail.com>
2024-09-28 22:05:41 +0530

    feat: :wrench: Add S3 configuration to config file

--- src/config/index.ts
+++ src/config/index.ts
@@ -8,7 +8,11 @@ const Config = {
     SERVER_URL: process.env.SERVER_URL,
     DATABASE_URL: process.env.DATABASE_URL,
     JWKS_URI: process.env.JWKS_URI,
-    BASE_URL: process.env.BASE_URL
+    BASE_URL: process.env.BASE_URL,
+    S3_BUCKET_NAME: process.env.S3_BUCKET_NAME,
+    S3_ACCESS_KEY: process.env.S3_ACCESS_KEY,
+    S3_SECRET_ACCESS_KEY: process.env.S3_SECRET_ACCESS_KEY,
+    S3_REGION: process.env.S3_REGION
 };
 
 export default Config;
