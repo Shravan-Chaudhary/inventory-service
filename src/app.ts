b6450488b80342533a600362585b2542e2e47484
Shravan <shravansinghchaudhary@gmail.com>
2024-10-11 20:05:59 +0530

    refactor: :recycle: Update health check route to use Config.BASE_URL

--- src/app.ts
+++ src/app.ts
@@ -27,17 +27,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "../public")));
 
 // Routes
-app.get("/", (req: Request, res: Response) => {
-    httpResponse(req, res, HttpStatus.OK, ResponseMessage.SUCCESS, {
-        port: Config.PORT
-    });
-});
-
 app.use(`${Config.BASE_URL}/categories`, categoryRouter);
 app.use(`${Config.BASE_URL}/products`, productRouter);
 
 // Health check
-app.get("/api/v1/health", (req: Request, res: Response) => {
+app.get(`${Config.BASE_URL}/health`, (req: Request, res: Response) => {
     const healthData = {
         application: getApplicationHealth(),
         system: getSystemHealth(),
